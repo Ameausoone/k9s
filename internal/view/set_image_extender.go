4237e2c7d39e8dc25fed2326f93172a04ccc9446
Antoine Meausoone <antoine.meausoone@gmail.com>
2020-10-15 22:50:11 +0200

    fix: replace unordered map by ImageSpec array
    fix comments issues

--- internal/view/set_image_extender.go
+++ internal/view/set_image_extender.go
@@ -11,23 +11,17 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+const setImageKey = "setImage"
+
 // SetImageExtender adds set image extensions
 type SetImageExtender struct {
 	ResourceViewer
 }
 
-type ContainerType string
-
-type ContainerImage struct {
-	ContainerType ContainerType
-	Image         string
+type imageFormSpec struct {
+	name, dockerImage, newDockerImage string
+	init                              bool
 }
-
-const (
-	setImageKey      = "setImage"
-	runningContainer = ContainerType("Container")
-	initContainer    = ContainerType("InitContainer")
-)
 
 func NewSetImageExtender(r ResourceViewer) ResourceViewer {
 	s := SetImageExtender{ResourceViewer: r}
@@ -68,26 +62,46 @@ func (s *SetImageExtender) showSetImageDialog(path string) {
 func (s *SetImageExtender) makeSetImageForm(sel string) *tview.Form {
 	f := s.makeStyledForm()
 	podSpec, err := s.getPodSpec(sel)
-	containers, initContainers := getImages(podSpec)
-	containersResult := make(map[string]string)
-	initContainersResult := make(map[string]string)
 	if err != nil {
 		s.App().Flash().Err(err)
 		return nil
 	}
-	addInputField(f, &containers, &containersResult)
-	addInputField(f, &initContainers, &initContainersResult)
+	var formResults []imageFormSpec
+	for i, spec := range podSpec.InitContainers {
+		formResults = append(formResults, imageFormSpec{init: true, name: spec.Name, dockerImage: spec.Image})
+		idxCopy := i
+		f.AddInputField(spec.Name, spec.Image, 0, nil, func(changed string) {
+			formResults[idxCopy].newDockerImage = changed
+		})
+	}
+	for i, spec := range podSpec.Containers {
+		formResults = append(formResults, imageFormSpec{init: false, name: spec.Name, dockerImage: spec.Image})
+		idxCopy := i
+		f.AddInputField(spec.Name, spec.Image, 0, nil, func(changed string) {
+			formResults[idxCopy].newDockerImage = changed
+		})
+	}
 
 	f.AddButton("OK", func() {
 		defer s.dismissDialog()
-
 		if err != nil {
 			s.App().Flash().Err(err)
 			return
 		}
 		ctx, cancel := context.WithTimeout(context.Background(), s.App().Conn().Config().CallTimeout())
 		defer cancel()
-		if err := s.setImages(ctx, sel, containersResult, initContainersResult); err != nil {
+		var imageSpecsResult dao.ImageSpecs
+		for _, input := range formResults {
+			if input.newDockerImage != "" && input.dockerImage != input.newDockerImage {
+				imageSpecsResult = append(imageSpecsResult, dao.ImageSpec{
+					Name:        input.name,
+					DockerImage: input.newDockerImage,
+					Init:        input.init,
+				})
+			}
+		}
+
+		if err := s.setImages(ctx, sel, imageSpecsResult); err != nil {
 			log.Error().Err(err).Msgf("PodSpec %s image update failed", sel)
 			s.App().Flash().Err(err)
 		} else {
@@ -97,32 +111,7 @@ func (s *SetImageExtender) makeSetImageForm(sel string) *tview.Form {
 	f.AddButton("Cancel", func() {
 		s.dismissDialog()
 	})
-
 	return f
-}
-
-func addInputField(f *tview.Form, containers *map[string]string, containersResult *map[string]string) {
-	for name, image := range *containers {
-		f.AddInputField(name, image, 0, nil, func(changed string) {
-			if changed == image {
-				delete(*containersResult, name)
-			} else {
-				(*containersResult)[name] = changed
-			}
-		})
-	}
-}
-
-func getImages(podSpec *corev1.PodSpec) (map[string]string, map[string]string) {
-	containers := make(map[string]string)
-	initContainers := make(map[string]string)
-	for _, c := range podSpec.Containers {
-		containers[c.Name] = c.Image
-	}
-	for _, c := range podSpec.InitContainers {
-		initContainers[c.Name] = c.Image
-	}
-	return containers, initContainers
 }
 
 func (s *SetImageExtender) dismissDialog() {
@@ -149,11 +138,10 @@ func (s *SetImageExtender) getPodSpec(path string) (*corev1.PodSpec, error) {
 	if !ok {
 		return nil, fmt.Errorf("expecting a resourceWPodSpec resource for %q", s.GVR())
 	}
-	podSpec, err := resourceWPodSpec.GetPodSpec(path)
-	return podSpec, nil
+	return resourceWPodSpec.GetPodSpec(path)
 }
 
-func (s *SetImageExtender) setImages(ctx context.Context, path string, containersPatch map[string]string, initContainersPatch map[string]string) error {
+func (s *SetImageExtender) setImages(ctx context.Context, path string, imageSpecs dao.ImageSpecs) error {
 	res, err := dao.AccessorFor(s.App().factory, s.GVR())
 	if err != nil {
 		return err
@@ -164,5 +152,5 @@ func (s *SetImageExtender) setImages(ctx context.Context, path string, container
 		return fmt.Errorf("expecting a scalable resource for %q", s.GVR())
 	}
 
-	return resourceWPodSpec.SetImages(ctx, path, containersPatch, initContainersPatch)
+	return resourceWPodSpec.SetImages(ctx, path, imageSpecs)
 }
